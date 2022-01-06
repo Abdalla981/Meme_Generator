19786a288e08f6bec834eb5ae1a665ade99db98b
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-06 21:43:38 +0100

    Added get_vocabulary_counter method that returns a counter of each vocab word in the dataset. This is helpful in removing captions with rare words in DatasetPreProcessor.py

--- utils/Dataset.py
+++ utils/Dataset.py
@@ -17,23 +17,20 @@ Methods:
 - show_samples: randomly selects images and writes a corresponding caption on them using MemeDrawer.py
 - save_captions_to_file: saves the captions to a new file
 - save_images_to_folder: saves the images to a new folder
+- get_vocabulary_counter: returns a counter of all vocabulary words
 '''
 class Dataset:
     def __init__(self, captions_path, images_path):
         self.image_path = images_path
         self.captions_path = captions_path
-        self.captions = self.load_captions()
+        self.captions, self.num_of_captions = self.load_captions()
         self.images, self.not_found_images = self.load_images()
         assert(len(self.images) == len(self.captions))
+        self.num_of_samples = len(self.images)
         
-    def get_captions_counter(self):
-        dataset_ditsribution = {
-            name: len(captions) for name, captions in self.captions.items()}
-        counter = Counter(dataset_ditsribution)
-        return counter
-
     def load_captions(self):
         data = {}
+        num_of_captions = 0
         if not os.path.exists(self.captions_path):
             raise FileNotFoundError(self.captions_path)
         with open(self.captions_path, 'r') as f:
@@ -45,7 +42,8 @@ class Dataset:
                 if name not in data:
                     data[name] = []
                 data[name].append(' '.join(line[1:]).strip())
-        return data
+                num_of_captions += 1
+        return data, num_of_captions
 
     def load_image(self, name):
         if not os.path.exists(self.image_path):
@@ -63,15 +61,36 @@ class Dataset:
             if image is None:
                 not_found.append((name, image_path))
         return data, not_found
+    
+    def get_captions_counter(self, captions_dict=None):
+        if captions_dict is None:
+            captions_dict = self.captions
+        dataset_ditsribution = {
+            name: len(captions) for name, captions in captions_dict.items()}
+        counter = Counter(dataset_ditsribution)
+        return counter
+    
+    def get_vocabulary_counter(self, captions_dict=None):
+        if captions_dict is None:
+            captions_dict = self.captions
+        words = []
+        [words.extend(caption.split()) 
+         for captions in captions_dict.values() for caption in captions]
+        vocab = Counter(words)
+        return vocab
 
-    def show_samples(self, num=5, color=(255, 255, 255), output_file_path=None):
+    def show_samples(self, images=None, captions=None, num=5, color=(255, 255, 255), output_file_path=None):
         names = [random.choice(list(self.captions.keys())) for i in range(num)]
+        if images is None:
+            images = self.images
+        if captions is None:
+            captions = self.captions
         for name in names:
-            caption = self.captions[name][0].split()
+            caption = captions[name][0].split()
             # split the text in half
             text1 = ' '.join(caption[:len(caption)//2])
             text2 = ' '.join(caption[len(caption)//2:])
-            image = self.images[name]
+            image = images[name]
             if image is not None:
                 img = MemeDrawer.ImageText(image)
                 # write the first half of the text on top
@@ -84,17 +103,22 @@ class Dataset:
                 if output_file_path is not None:
                     img.save(os.path.join(output_file_path, name + '.jpg'))
 
-    def save_captions_to_file(self, new_captions_path):
+    def save_captions_to_file(self, new_captions_path, captions_dict=None):
         if new_captions_path == self.captions_path:
             raise ValueError('Can not write to same file!')
+        if captions_dict is None:
+            captions_dict = self.captions
         caption_file = open(new_captions_path, 'w')
-        for name, captions in self.captions.items():
+        for name, captions in captions_dict.items():
             for caption in captions:
                 caption_file.write(f'{name} - {caption}\n')
         caption_file.close()
         
-    def save_images_to_folder(self, new_images_path):
+    def save_images_to_folder(self, new_images_path, images_dict=None):
         if new_images_path == self.image_path:
             raise ValueError('Can not write to same folder!')
-        for name, image in self.images.item():
+        if images_dict is None:
+            images_dict = self.images
+        for name, image in images_dict.item():
             image.save(os.path.join(new_images_path, name + '.jpg'))
+            
