36e76e6e4c52651f87cf1e0c29a1f355f5a6ef48
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-09 06:21:02 +0100

    Added short description to the two files

--- compute_embeddings.py
+++ compute_embeddings.py
@@ -2,18 +2,24 @@ from pickle import load
 from preprocessing.EfficientNet import EfficientNet
 from preprocessing.Glove import Glove
 
+'''
+This script runs the EfficientNet class and the Glove class for pre-calculation of embeddings
+'''
+
 if __name__ == '__main__':
     images_folder = 'dataset/memes3'
     images_output_folder = 'embedding/efficientNet'
     glove_path = 'embedding/glove/glove.42B.300d.txt'
     captions_folder = 'dataset/CaptionsClean3.txt'
     glove_output_file = 'embedding/glove/captionsGlove.pkl'
+    not_found_word_file = 'embedding/glove/notFoundWords.txt'
     
     if input('Run EfficientNet?(y/n) ') == 'y':
         image_embedding_obj = EfficientNet(images_folder, models_list=('B0', 'B2'))
         image_embedding_obj.extract_features()
         image_embedding_obj.save_features_to_file(images_output_folder)
+        
     if input('Run Glove?(y/n) ') == 'y':
         glove_obj = Glove(captions_folder, glove_path)
-        glove_obj.save_vocab_embedding_to_file(glove_output_file)
+        glove_obj.save_vocab_embedding_to_file(glove_output_file, not_found_word_file)
     
--- load_dataset.py
+++ load_dataset.py
@@ -2,21 +2,23 @@ from preprocessing.CaptionsCleaning import CaptionsCleaning
 from training.DatasetProcessor import DatasetProcessor
 
 '''
-This script loads the dataset and pre-processes it and writes the clean dataset captions to a new file.
-The new dataset captions and corresponding images are loaded and their info is printed
+This script loads the dataset and pre-processes it and writes the clean captions captions to a new file.
+The new dataset is loaded and its info is printed.
 '''
 if __name__ == '__main__':
     captions_path = 'dataset/Captions3.txt'
     images_path = 'embedding/efficientNet'
     new_captions_path = 'dataset/CaptionsClean3.txt'
+    embeddings_path = 'embedding/glove/captionsGlove.pkl'
     
     if input('Clean Dataset?(y/n) ') == 'y':
         clean_obj = CaptionsCleaning(captions_path)
         clean_obj.clean_dataset()
         clean_obj.save_captions_to_file(new_captions_path)
     
-    process_obj = DatasetProcessor(new_captions_path, images_path, 'B0')
-    process_obj.process_dataset()
-    process_obj.print_dataset_info()
-    process_obj.print_sample_of_sequences()
+    if input('Load dataset?') == 'y':
+        process_obj = DatasetProcessor(new_captions_path, images_path, 'B0', embeddings_path)
+        process_obj.process_dataset()
+        process_obj.print_dataset_info()
+        process_obj.print_sample_of_sequences()
     
