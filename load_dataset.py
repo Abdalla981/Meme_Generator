d4c6c71adeb51a03e66d8ebf26221b35283eae02
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-09 03:16:15 +0100

    Updated the files path according to latest changes

--- load_dataset.py
+++ load_dataset.py
@@ -1,32 +1,22 @@
-from dataset.DatasetPreProcessor import DatasetPreProcessor
-from dataset.DatasetProcessor import DatasetProcessor
+from preprocessing.CaptionsCleaning import CaptionsCleaning
+from training.DatasetProcessor import DatasetProcessor
 
 '''
 This script loads the dataset and pre-processes it and writes the clean dataset captions to a new file.
 The new dataset captions and corresponding images are loaded and their info is printed
 '''
 if __name__ == '__main__':
-    captions_path = 'Captions3.txt'
-    images_path = 'memes3'
-    new_captions_path = 'CaptionsClean3.txt'
+    captions_path = 'dataset/Captions3.txt'
+    images_path = 'embedding/efficientNet'
+    new_captions_path = 'dataset/CaptionsClean31.txt'
     
     if input('Clean Dataset?(y/n) ') == 'y':
-        prep_obj = DatasetPreProcessor(captions_path, images_path)
+        prep_obj = CaptionsCleaning(captions_path)
         prep_obj.clean_dataset()
         prep_obj.save_captions_to_file(new_captions_path)
     
-    process_obj = DatasetProcessor(new_captions_path, images_path)
+    process_obj = DatasetProcessor(new_captions_path, images_path, 'B2')
     process_obj.process_dataset()
     process_obj.print_dataset_info()
     process_obj.print_sample_of_sequences()
-    if input('Show a training images sample?(y/n) ') == 'y':
-        process_obj.show_samples(captions_dict=process_obj.train_captions, 
-                                 images_dict=process_obj.train_images)
-    if input('Show a validation images sample?(y/n) ') == 'y':
-        process_obj.show_samples(captions_dict=process_obj.validation_captions, 
-                                 images_dict=process_obj.validation_images)
-    if input('Show a test images sample?(y/n) ') == 'y':
-        process_obj.show_samples(captions_dict=process_obj.test_captions, 
-                                 images_dict=process_obj.test_images)
-    
     
