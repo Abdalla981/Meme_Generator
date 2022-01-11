f3faba0507fb90326fca3f460aeb3032854f369c
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-11 03:37:59 +0100

    Removed load_dataset.py and added clean_dataset.py and split_dataset.py instead to make code cleaner. Updated .gitignore to ignore models (files too large)

---
+++ clean_dataset.py
@@ -0,0 +1,14 @@
+from preprocessing.CaptionsCleaning import CaptionsCleaning
+
+'''
+This script loads the dataset and pre-processes it and 
+writes the clean captions captions to a new file.
+'''
+if __name__ == '__main__':
+    captions_path = 'dataset/Captions3.txt'
+    new_captions_path = 'dataset/CaptionsClean3.txt'
+    
+    if input('Clean Dataset?(y/n) ') == 'y':
+        clean_obj = CaptionsCleaning(captions_path)
+        clean_obj.clean_dataset()
+        clean_obj.save_captions_to_file(new_captions_path)
---
+++ split_dataset.py
@@ -0,0 +1,18 @@
+import os
+from preprocessing.CaptionsSplitter import CaptionsSplitter
+
+if __name__ == '__main__':
+    captions_path = 'dataset/CaptionsClean3.txt'
+    train_captions_path = 'dataset/CaptionsClean3_train.txt'
+    val_captions_path = 'dataset/CaptionsClean3_validation.txt'
+    test_captions_path = 'dataset/CaptionsClean3_test.txt'
+    
+    if input('Split Dataset?(y/n) ') == 'y':
+        splitter_obj = CaptionsSplitter(captions_path)
+        splitter_obj.add_tokens_to_caption()
+        train_captions, val_captions, test_captions = splitter_obj.split_dataset()
+        splitter_obj.print_dataset_info()
+        splitter_obj.save_captions_to_file(train_captions_path, train_captions)
+        splitter_obj.save_captions_to_file(val_captions_path, val_captions)
+        splitter_obj.save_captions_to_file(test_captions_path, test_captions)
+        
