762ed44c709e60b8a5a7fa67757c3d98aae40c58
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-23 20:09:14 +0100

    Added a name parameter to save BLEU and save captions. Enabled verbosity paramter.

--- testing/EvaluateModel.py
+++ testing/EvaluateModel.py
@@ -6,8 +6,8 @@ from typing import Tuple
 from tensorflow import reshape as tf_reshape
 from keras.preprocessing.sequence import pad_sequences
 from nltk.translate.bleu_score import corpus_bleu
+from training.DatasetProcessor import DatasetProcessor
 from training.MergeModel import MergeModel
-from utils.MemeDrawer import ImageText
 from training.Dataset import Dataset
 
 '''
@@ -27,10 +27,11 @@ the reference captions of that image for BLEU score later
 '''
 
 class EvaluateModel():
-    def __init__(self, model_obj: MergeModel, images_path: str, num: int=None, k: int=5,
-                 eval: str='beam') -> None:
+    def __init__(self, model_obj: MergeModel, images_path: str, dp_obj: DatasetProcessor=None, num: int=None, k: int=5,
+                 eval: str='beam', verbose: int=0) -> None:
         self.model_obj = model_obj
-        self.dp_obj = model_obj.dp_obj
+        self.dp_obj = model_obj.dp_obj if dp_obj is None else dp_obj
+        self.verbose = verbose
         self.num = num
         self.k = k
         self.eval = eval
@@ -52,7 +53,8 @@ class EvaluateModel():
             refrences = [caption.split() for caption in captions]
             og_captions.append(refrences)
             gen_captions[name] = gen_c.split()
-            print(f'{i}. {name}')
+            if self.verbose:
+                print(f'{i}. {name}')
         return og_captions, gen_captions
             
     def greedy_search(self, image) -> str:
@@ -99,19 +101,21 @@ class EvaluateModel():
             sequences = sorted(candidates, key=lambda tup:tup[1])[-self.k:]
         return sequences[-1][0]
         
-    def save_BLEU_evaluation(self) -> None:
+    def save_BLEU_evaluation(self, name) -> None:
         b1 = corpus_bleu(self.og_captions, list(self.gen_captions.values()), weights=(1.0, 0, 0, 0))
         b2 = corpus_bleu(self.og_captions, list(self.gen_captions.values()), weights=(0.5, 0.5, 0, 0))
         b3 = corpus_bleu(self.og_captions, list(self.gen_captions.values()), weights=(0.3, 0.3, 0.3, 0))
         b4 = corpus_bleu(self.og_captions, list(self.gen_captions.values()), weights=(0.25, 0.25, 0.25, 0.25))
-        result_str = f'BLEU-1: {b1}\nBLEU-2: {b2}\nBLEU-3: {b3}\nBLEU-4: {b4}\n'
+        print(self.dp_obj.get_vocabulary_counter(self.gen_captions))
+        vocab = len(self.dp_obj.get_vocabulary_counter(self.gen_captions))
+        result_str = f'BLEU-1: {b1}\nBLEU-2: {b2}\nBLEU-3: {b3}\nBLEU-4: {b4}\nNumber of Vocabulary: {vocab}\n'
         print(result_str)
-        f_path = os.path.join(self.model_obj.model_folder, 'BLEU_result.txt')
+        f_path = os.path.join(self.model_obj.model_folder, name + 'BLEU_result.txt')
         with open(f_path, 'w') as f:
             f.write(result_str)
     
-    def save_captions(self) -> None:
-        f_path = os.path.join(self.model_obj.model_folder, 'generated_captions.json')
+    def save_captions(self, name) -> None:
+        f_path = os.path.join(self.model_obj.model_folder, name + 'generated_captions.json')
         with open(f_path, 'w') as f:
             json.dump(self.gen_captions, f)
             
