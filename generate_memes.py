7d5e5246d2fa7d5785f3e030e9c6f574456e0b3b
Abdalla981 <abdalla.moustafa981@gmail.com>
2022-01-24 20:36:10 +0100

    Added image names to obtain the memes

--- generate_memes.py
+++ generate_memes.py
@@ -2,12 +2,19 @@ from training.Dataset import Dataset
 from json import load
 
 if __name__ == '__main__':
-    gen_captions_path = 'models/BaseModel/generated_captions_B6.json'
+    gen_captions_path = 'models/BestModel/B0/generated_captions.json'
     images_path = 'dataset/memes3'
-    output_folder = 'Meme_drawer/Model_generated/BaseModel/B6'
+    output_folder = 'models/BestModel/B0/samples'
+    template_names = ['MrBrut', 'Gamer_rage', 'Idiot_Football_Coach', 
+                      'Counter_Strike', 'Success_Rdj', 'Jimmy_Pulp_Fiction', 
+                      'Musically_Diverse_Metalhead', 'Look_at_all_the_things', 'truestoryxd',
+                      'Cool_Drum_Corps', 'Uneducatedchicken', 'Trans_Parrotfish']
     
+    sel_captions = {}
     with open(gen_captions_path, 'r') as f:
         captions = load(f)
-        
+    for name in template_names:
+        sel_captions[name] = captions[name]
     d_obj = Dataset(images_path=images_path)
-    d_obj.show_samples(captions_dict=captions, splitted=True, num=10, output_file_path=output_folder)
+    d_obj.show_samples(captions_dict=sel_captions, splitted=True, output_file_path=output_folder, 
+                       num=len(template_names), random=False)
